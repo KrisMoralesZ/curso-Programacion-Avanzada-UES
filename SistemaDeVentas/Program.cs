17fb393e7e92384aa18ebc660ef1fed740ed1c00
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2026-03-14 17:05:13 -0700

    MySQL Database Connection

--- SistemaDeVentas/Program.cs
+++ SistemaDeVentas/Program.cs
@@ -1,22 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace SistemaDeVentas
 {
-    internal static class Program
+  internal static class Program
+  {
+    [STAThread]
+    static void Main()
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
+      string conexion = "Server=127.0.0.1;Database=SistemaDeVentas;Uid=root;Pwd=;";
+
+      using (MySqlConnection conn = new MySqlConnection(conexion))
+      {
+        conn.Open();
+
+      }
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new Form1());
+
     }
+  }
 }
