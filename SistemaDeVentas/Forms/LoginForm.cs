bde930c35f520b7969b32f331bedd9463b937e47
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2026-03-15 19:57:28 -0700

    Clean unused imports

--- SistemaDeVentas/Forms/LoginForm.cs
+++ SistemaDeVentas/Forms/LoginForm.cs
@@ -1,16 +1,7 @@
 ﻿using SistemaDeVentas.Forms;
 using SistemaDeVentas.Data;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Drawing.Text;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace SistemaDeVentas
 {
